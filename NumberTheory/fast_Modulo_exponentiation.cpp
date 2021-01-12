8c93224969ba9654b5baaaf3bb3ca7fdff810df1
Susheel Arukonda <asusheel23b@gmail.com>
2021-01-12 12:12:34 +0530

    Using Bitmasks

---
+++ NumberTheory/fast_Modulo_exponentiation.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+
+ll Power(int a,int b)
+{
+	ll res=1;
+	while(b){
+	if(b&1)
+	{
+		res=((res)*(a))%mod;
+	}
+	b=b>>1;
+	a=((a)*(a))%mod;
+    }
+    return res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+	int n;
+	cin>>n;
+	while(n--){
+	ll a,b;
+	cin>>a>>b;
+	cout<<Power(a,b)<<endl;
+}
+}
