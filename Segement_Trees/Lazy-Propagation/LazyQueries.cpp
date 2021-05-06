4e51e2772f96527c350f1410f02094d85cf544bc
Susheel Arukonda <asusheel23b@gmail.com>
2021-05-06 12:06:12 +0530

    Lazy queries and updates

--- Segement_Trees/Lazy-Propagation/LazyQueries.cpp
+++ Segement_Trees/Lazy-Propagation/LazyQueries.cpp
@@ -45,6 +45,41 @@ void updateRangeLazy(int *tree,int ss,int se ,int l,int r,int inc,int index)
 	//update the tree[index]
 	tree[index] = min(tree[2*index],tree[2*index+1]);
 
+}
+
+int queryLazy(int *tree,int ss,int se,int qs,int qe,int index)
+{
+	//resolve lazy value before going down if exists
+	if(lazy[index]!=0)
+	{
+		tree[index]+=lazy[index];
+
+		//non-leaf node
+		if(ss!=se)
+		{
+			lazy[2*index] += lazy[index];
+			lazy[2*index+1] += lazy[index];
+		}
+		lazy[index] = 0;            //clear the lazy value at certain node
+	}
+
+	//Query logic
+	if(ss>qe || se<qs)         //No overlap
+		return INT_MAX;
+
+	//complete overlap
+	if(ss>=qs and se<=qe)
+	{
+		return tree[index];
+	}
+
+	//partial overlap
+	int mid= (ss+se)/2;
+
+	int left = queryLazy(tree,ss,mid,qs,qe,2*index);
+	int right = queryLazy(tree,mid+1,se,qs,qe,2*index+1);
+
+	return min(left,right);
 
 
 }
@@ -78,9 +113,19 @@ int main()
 
 	buildtree(a,0,n-1,tree,1);
 
-	for(int i=1;i<=13;i++)
-	{
-		cout<<tree[i]<<" ";
-	}
-	cout<<endl;
+	updateRangeLazy(tree,0,n-1,0,2,10,1);
+	updateRangeLazy(tree,0,n-1,0,4,10,1);
+	cout<<"Q1 1-1 -> "<<queryLazy(tree,0,n-1,1,1,1)<<endl;
+
+	updateRangeLazy(tree,0,n-1,3,4,10,1);
+
+	cout<<"Q1 3-5 -> "<< queryLazy(tree,0,n-1,3,5,1);
+
+
+
+	// for(int i=1;i<=13;i++)
+	// {
+	// 	cout<<tree[i]<<" ";
+	// }
+	// cout<<endl;
 }
