b73719d9af630884d55e248599aa31d5a3dec802
Susheel Arukonda <asusheel23b@gmail.com>
2021-05-18 11:42:43 +0530

    square root of number using BS

---
+++ Divide & conquer/monotonic.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define fi first;
+#define se second;
+#define pb push_back;
+#define mp make_pair;
+#define fo(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define fon(a, b, c) for (int(a) = (b); (a) <= (c); ++(a)) 
+#define fod(a, b, c) for (int(a) = (b); (a) >= (c); --(a)) 
+#define fosq(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a)) 
+#define foc(a, b, c) for (char(a) = (b); (a) <= (c); ++(a)) 
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define SORT(v) sort(all(v))
+#define TC(t) while (t--)
+#define PI 3.1415926535897932384626
+const int M=1e9+7;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int solve()
+{
+	int n;
+	cin>>n;
+	int s=0,e=n,ans=-1;
+	while(s<=e)
+	{
+		int mid=(s+e)/2;
+
+		if(mid*mid==n)
+			return mid;
+
+		else if(mid*mid>n)
+		{
+			e=mid-1;
+		}
+		else
+		{
+			ans=mid;
+			s=mid+1;
+		}
+	}
+	return ans;
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL); 
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+	int t=1;
+	cin>>t;
+	while(t--)
+	{
+	   cout<<solve()<<endl;
+	}
+	
+}
