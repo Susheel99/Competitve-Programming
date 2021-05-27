d4f82ccf10f654c8eeb99bc744ba7c71b525a1f4
Susheel Arukonda <asusheel23b@gmail.com>
2021-05-27 10:24:45 +0530

    interaction guide

--- interactive.cpp.cpp
+++ interactive.cpp.cpp
@@ -27,60 +27,33 @@ typedef vector<pl> vpl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 
-int solve(vector<int> a,int s,int e,int key)
+int solve()
 {
-	while(s<=e)
-	{
-		int mid=(s+e)/2;
-
-		if(a[mid]==key)
-			return mid;
-
-		else if(a[s]<=a[mid])
-		{
-			if(a[s]<=key and key<=a[mid])
-			{
-				e=mid-1;
-			}
-			else
-			{
-				s=mid+1;
-			}
-		}
-		else //if(a[e]>=a[mid])
-		{
-			if(a[e]>=key and key>=a[mid])
-			{
-				s=mid+1;
-			}
-			else
-			{
-				e=mid-1;
-			}
-
-		}
-	}
-
-	return -1;
+	return 0;
 }
 
 
 int main()
 {
     ios_base::sync_with_stdio(false); 
-    cin.tie(NULL); 
+    cin.tie(NULL);cout.tie(NULL);      
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
 	int t=1;
 	cin>>t;
 	while(t--)
 	{
-		int n,key;
-		cin>>n>>key;
-		vi v(n);
-		fo(i,0,n) cin>>v[i];
-
-
-	   cout<<solve(v,0,n-1,key)<<endl;
+		int n;
+		cin>>n;
+		cout<<"This is n"<<n<<'\n';
+		cout<<flush;
+	   //solve();
 	}
 	
 }
+//use flush or endl with cout in inetarctive problems if line 38,39 are included
+//endl works as cout<<'\n'<<flush;
+
+//fflush(stdout) in c++, if using printf and scanf
+//cout<<flush; in c++ if using cin,cout
+//System.out.flush() in Java
+//stdout.flush() in Python
