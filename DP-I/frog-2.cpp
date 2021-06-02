facacadd9c486dccd8bc1d719e499e07f0020864
Susheel Arukonda <asusheel23b@gmail.com>
2021-06-02 11:41:34 +0530

    bottom up dp

---
+++ DP-I/frog-2.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define fi first;
+#define se second;
+#define pb push_back;
+#define mp make_pair;
+#define fo(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define fon(a, b, c) for (int(a) = (b); (a) <= (c); ++(a)) 
+#define fod(a, b, c) for (int(a) = (b); (a) >= (c); --(a)) 
+#define fosq(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a)) 
+#define foc(a, b, c) for (char(a) = (b); (a) <= (c); ++(a)) 
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define SORT(v) sort(all(v))
+#define TC(t) while (t--)
+#define PI 3.1415926535897932384626
+const int M=1e9+7;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int solve()
+{
+	int n,k;
+	cin>>n>>k;
+	ll h[n+1] {0};
+	ll dp[n+1] {0};
+
+	for(int i=1;i<=n;i++) cin>>h[i];
+
+	dp[1]=0;
+	dp[2]=abs(h[2]-h[1]);
+
+	
+	for(int i=3;i<=n;i++)
+	{
+		ll ans=INT_MAX;
+		for(int j=1;j<=k;j++)
+		{
+			if(i>=j)
+			{
+				ans=min(ans,dp[i-j]+abs(h[i]-h[i-j]));
+			}
+			else
+				break;
+		}
+		dp[i]=ans;
+		// dp[i]=min(dp[i-1]+abs(h[i]-h[i-1]),dp[i-2]+abs(h[i]-h[i-2]));
+	}
+	return dp[n];
+
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);cout.tie(NULL);      
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+	int t=1;
+	cin>>t;
+	while(t--)
+	{
+	   cout<<solve()<<endl;
+	}
+	
+}
